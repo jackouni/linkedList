1585c7394d2de1b5d18c5cbe12e5da45d1c27053
Jack Sebben <jacksebben11@hotmail.com>
2023-12-07 21:16:48 -0500

    First commit. I got carried away here and didn't commit or comment along the way. Made the append, prepend, size, head and tail methods for the Linked List object. Will continue the rest tomorrow.

---
+++ script.js
@@ -0,0 +1,84 @@
+function createLinkedList(value) {
+	function append(value) {
+		let currentNode = headNode;
+
+		while (currentNode.nextNode != null) {
+			currentNode = currentNode.nextNode;
+		}
+
+		currentNode.nextNode = createNode(value);
+	}
+
+	function prepend(value) {
+		let newHead = createNode(value);
+		newHead.nextNode = headNode;
+		headNode = newHead;
+	}
+
+	function size() {
+		let count = 1;
+		let currentNode = headNode;
+
+		while (currentNode.nextNode != null) {
+			count++;
+			currentNode = currentNode.nextNode;
+		}
+
+		return count;
+	}
+
+	function head() {
+		return headNode;
+	}
+
+	function tail() {
+		let currentNode = headNode;
+
+		while (currentNode.nextNode != null) {
+			currentNode = currentNode.nextNode;
+		}
+
+		return currentNode;
+	}
+
+	function at(index) {}
+
+	function pop() {}
+
+	function contains(value) {}
+
+	function find(value) {}
+
+	function toString() {}
+
+	let headNode = createNode(value);
+
+	return {
+		get headNode() {
+			return headNode;
+		},
+		append,
+		prepend,
+		size,
+		head,
+		tail,
+		at,
+		pop,
+		contains,
+		find,
+		toString,
+	};
+}
+
+function createNode(value) {
+	return {
+		value: value,
+		nextNode: null,
+	};
+}
+
+let myList = createLinkedList(10);
+myList.append(101);
+myList.append(22);
+myList.append(11);
+console.log(myList.head());
